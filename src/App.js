edd04ba9423dd68bcf3e392aa7d3e6c74fddf887
Hasan Kara <hassanoguzkara2009@hotmail.com>
2020-09-25 01:30:41 +0300

    cart component has been created, and has functionalty

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 
@@ -8,15 +9,16 @@ const App = () => {
   const INITIAL_STATE = {
     products: data.products,
     size: "",
+    cartItems: [],
     sort: "",
   };
 
   const [state, setState] = useState(INITIAL_STATE);
 
   const sortProducts = (event) => {
-    console.log(event.target.value);
     const sort = event.target.value;
     setState({
+      ...state,
       sort: sort,
       products: state.products
         .slice()
@@ -37,9 +39,12 @@ const App = () => {
   };
 
   const filterProducts = (event) => {
-    console.log(event.target.value);
     event.target.value === "ALL"
-      ? setState({ size: event.target.value, products: data.products })
+      ? setState({
+          ...state,
+          size: event.target.value,
+          products: data.products,
+        })
       : setState({
           ...state,
           size: event.target.value,
@@ -47,6 +52,24 @@ const App = () => {
             (product) => product.availableSizes.indexOf(event.target.value) >= 0
           ),
         });
+  };
+
+  const addToCart = (product) => {
+    setState({
+      ...state,
+      cartItems: state.cartItems.find((item) => item._id === product._id)
+        ? state.cartItems.map((item) =>
+            item._id === product._id ? { ...item, count: item.count + 1 } : item
+          )
+        : [...state.cartItems, { ...product, count: 1 }],
+    });
+  };
+
+  const removeFromCart = (product) => {
+    setState({
+      ...state,
+      cartItems: state.cartItems.filter((item) => item._id !== product._id),
+    });
   };
 
   return (
@@ -60,12 +83,17 @@ const App = () => {
             <Filter
               count={state.products.length}
               sort={state.sort}
-              filterProducts={(e) => filterProducts(e)}
-              sortProducts={(e) => sortProducts(e)}
+              filterProducts={filterProducts}
+              sortProducts={sortProducts}
             />
-            <Products products={state.products} />
+            <Products
+              products={state.products}
+              addToCart={(e) => addToCart(e)}
+            />
           </div>
-          <div className="sidebar">Card items</div>
+          <div className="sidebar">
+            <Cart cartItems={state.cartItems} removeFromCart={removeFromCart} />
+          </div>
         </div>
       </main>
       <footer>All right is reserved.</footer>
